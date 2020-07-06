efea05f03ff82538c840a5015df9adccfe18f6a6
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-06 22:31:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
 def reverse_each_word(new_hash)
-   new_hash.to_s
+   new_datum = new_hash.inject({}) do |converted_datum, (key, value)|
+  converted_datum[key] = value.to_s
+
+ converted_datum
+end
   end
