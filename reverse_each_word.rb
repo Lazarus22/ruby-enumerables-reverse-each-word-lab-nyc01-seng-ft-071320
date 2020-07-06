d6b737d11709a5c1668c130ee81e0aae5b47622c
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-06 22:42:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
 def reverse_each_word(new_hash)
     new_hash.split.map {|word| word.reverse}.join(" ")
   end
+
+  def reverse_each_word(new_hash)
+      new_hash.split.collect {|word| word.reverse}.join(" ")
+    end
